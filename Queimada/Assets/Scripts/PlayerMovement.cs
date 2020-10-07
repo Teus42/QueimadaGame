c9b7b5aec6a69271906e4d495c6f57295ba7bd03
leonardoreali <l.reali@hotmail.com>
2020-10-07 20:38:03 -0300

    Nem tem
    PowerUP Jump e Speed.

--- Queimada/Assets/Scripts/PlayerMovement.cs
+++ Queimada/Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,13 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Character Settings")]   
+    [Header("Character Settings")]
     //public CharacterController controller;    
     public Transform groundCheck;
     public float groundDistance = 0.4f;
-    public LayerMask groundMask;   
+    public LayerMask groundMask;
     private Rigidbody _rb;
-    private Transform playerTransform;   
+    private Transform playerTransform;
     private bool isGrounded;
     private Vector3 velocity;
 
@@ -21,71 +21,84 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask playerMask;
 
     [SerializeField]
-    private bool isOnArea;     
-    
+    private bool isOnArea;
+
     [Header("Movement Settings")]
+    public bool _onSuperSpeed = false;
     public float speed = 4f;
-    public float jump = 6f; 
+    public bool _onSuperJump = false;
+    public float jump = 6f;
     public Animator _anim;
-    private Vector3 inputMove; 
-    private Vector3 movement; 
+    private Vector3 inputMove;
+    private Vector3 movement;
     private GameObject mainCamera;
     private bool isWalking;
     private bool isRunning;
-     
 
-    
+
+
     void Start()
     {
+
         _rb = this.GetComponent<Rigidbody>();
-        playerTransform = this.GetComponent<Transform>();        
-        mainCamera = GameObject.Find("Main Camera"); 
-          
+        playerTransform = this.GetComponent<Transform>();
+        mainCamera = GameObject.Find("Main Camera");
+
     }
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance,groundMask);  
-        if(isGrounded)
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded)
         {
-            _anim.SetBool("Grounded",true);
-        }else
+            _anim.SetBool("Grounded", true);
+        }
+        else
         {
-           _anim.SetBool("Grounded",false);
+            _anim.SetBool("Grounded", false);
         }
     }
 
     private void FixedUpdate()
-    {   
+    {
         MovementPlayer();
         Run();
-        OnAreaBall();       
+        OnAreaBall();
     }
 
     private void OnMovement(InputValue value)
-    {   
+    {
         var v = value.Get<Vector2>();
-        inputMove = new Vector3(v.x,0,v.y);                    
+        inputMove = new Vector3(v.x, 0, v.y);
     }
 
     private void OnJump(InputValue value)
     {
-        if(isGrounded)
+        if (isGrounded)
         {
-            _rb.AddForce(Vector3.up * _rb.velocity.y, ForceMode.VelocityChange);
-            _rb.AddForce(Vector3.up * jump, ForceMode.VelocityChange);            
+            if (_onSuperJump == false)
+            {
+                _rb.AddForce(Vector3.up * _rb.velocity.y, ForceMode.VelocityChange);
+                _rb.AddForce(Vector3.up * jump, ForceMode.VelocityChange);
+            }
+            else if (_onSuperJump == true)
+            {
+                _rb.AddForce(Vector3.up * _rb.velocity.y, ForceMode.VelocityChange);
+                _rb.AddForce(Vector3.up * jump * 3, ForceMode.VelocityChange);
+            }
         }
     }
     private void OnRun(InputValue value)
-    {  
-        if(value.isPressed)
+    {
+        if (value.isPressed)
         {
             isRunning = true;
-        }else
+        }
+        else
         {
             isRunning = false;
         }
-    }      
-       
+    }
+
 
     private void OnShoot()
     {
@@ -93,53 +106,91 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void MovementPlayer()
-    {         
-        movement = new Vector3(inputMove.x,0f,inputMove.z);    
-        movement = movement * speed * Time.deltaTime;
+    {
+        movement = new Vector3(inputMove.x, 0f, inputMove.z);
+
+        if (_onSuperSpeed == false)
+        {
+            movement = movement * speed * Time.deltaTime;
+        }
+        else if (_onSuperSpeed == true)
+        {
+            movement = movement * speed * 2 * Time.deltaTime;
+        }
+
+
         float facing = mainCamera.transform.eulerAngles.y;
 
         var cam = mainCamera.transform.forward;
-        Debug.DrawRay(mainCamera.transform.position,cam * 12, Color.red);   
+        Debug.DrawRay(mainCamera.transform.position, cam * 12, Color.red);
 
-        Vector3 _relativeMovement = Quaternion.Euler(0,facing,0) * movement;
+        Vector3 _relativeMovement = Quaternion.Euler(0, facing, 0) * movement;
 
-        _rb.MovePosition(playerTransform.position + _relativeMovement);        
-        if(inputMove.x != 0 || inputMove.z != 0)
+        _rb.MovePosition(playerTransform.position + _relativeMovement);
+        if (inputMove.x != 0 || inputMove.z != 0)
         {
-            _anim.SetBool("Walk",true);
-        }else
+            _anim.SetBool("Walk", true);
+        }
+        else
         {
-            _anim.SetBool("Walk",false);
+            _anim.SetBool("Walk", false);
         }
     }
 
     void Run()
     {
-        if(isRunning)
+        if (isRunning)
         {
             speed = 12f;
-            _anim.SetBool("Run",true);            
-        }else
+            _anim.SetBool("Run", true);
+        }
+        else
         {
             speed = 4f;
-            _anim.SetBool("Run",false);
+            _anim.SetBool("Run", false);
         }
     }
 
     void OnAreaBall()
     {
-        isOnArea = Physics.CheckSphere(areaCheck.position,areaDistance,playerMask);
+        isOnArea = Physics.CheckSphere(areaCheck.position, areaDistance, playerMask);
 
-        if(isOnArea)
+        if (isOnArea)
         {
             Debug.Log("Perto da bola");
-        }else
+        }
+        else
         {
             Debug.Log("Longe da bola");
         }
     }
 
-    
-    
+    // Corrotinas
+
+    /* Super Speed  */
+    public void OnSpeed()
+    {
+
+        StartCoroutine(SpeedCorroutine());
+    }
+    public IEnumerator SpeedCorroutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        _onSuperSpeed = false;
+    }
+
+
+    /* Super Jump  */
+    public void OnJump()
+    {
+
+        StartCoroutine(JumpCorroutine());
+    }
+    public IEnumerator JumpCorroutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        _onSuperJump = false;
+    }
 
 }
+
---
+++ Queimada/Assets/Scripts/Poderzinho.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poderzinho : MonoBehaviour
+{
+
+    public int id;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerMovement player = other.GetComponent<PlayerMovement>();
+
+        if (id == 1)
+        {
+            player._onSuperSpeed = true;
+            player.OnSpeed();
+        }
+        else if (id == 2)
+        {
+            player._onSuperJump = true;
+            player.OnJump();
+        }
+
+
+        Destroy(this.gameObject);
+    }
+}
+
